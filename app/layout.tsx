142abdbcea8ba9bfeca26df3579eb00604993dee
Bertrand Ond <bertrand.ond@email.com>
2025-12-15 18:17:11 -0500

    chore: display temporary maintenance homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 export const metadata = {
   title: "Sawaka",
-  description: "Artisanat authentique"
+  description: "Artisanat authentique",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
